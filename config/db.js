033e82d13e43f1fedca6b4ead506049ef6280fae
icyflamex25 <siddhant25062002@gmail.com>
2022-12-06 11:04:42 +0530

    added all files

--- config/db.js
+++ config/db.js
@@ -3,23 +3,20 @@ const mongoose = require('mongoose')
 
 function connectDB() {
 
-  const connectionString = process.env.MONGODB_CONNECTION_URL;
-  mongoose.connect(
-    connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  }
-  );
+    const connectionString = process.env.MONGODB_CONNECTION_URL;
+    mongoose.connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+    });
 
-  const connection = mongoose.connection;
-  connection.once('open', () => {
-    console.log(`Database connected`);
-  })
-    .catch((err) => {
-      console.log('Database connection failed', err);
-      process.exit(1);
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+        console.log(`Database connected`);
+    }).catch((err) => {
+        console.log('Database connection failed', err);
+        process.exit(1);
     });
 }
 
