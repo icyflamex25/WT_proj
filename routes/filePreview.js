6a18ae2c01ae52d15019e35e1df44a6d9e85a792
icyflamex25 <76946103+icyflamex25@users.noreply.github.com>
2022-12-06 17:51:51 +0530

    Update filePreview.js

--- routes/filePreview.js
+++ routes/filePreview.js
@@ -15,7 +15,7 @@ router.get('/:uuid', async (req, res) => {
       uuid: file.uuid,
       filename: file.filename,
       size: file.size,
-      downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
+      downloadLink: `https://simplyshare.onrender.com/files/download/${file.uuid}`
     })
   } catch (error) {
     return res.render('download', {
