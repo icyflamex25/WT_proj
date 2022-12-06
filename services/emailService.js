c441c42dac84c2a933ba126e60fc11d10233e393
icyflamex25 <76946103+icyflamex25@users.noreply.github.com>
2022-12-06 19:29:40 +0530

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -19,7 +19,7 @@ module.exports = async ({
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: `FileShare <${from}>`,
+    from: `SimplyShare <${from}>`,
     to: to,
     subject: subject,
     text: text,
